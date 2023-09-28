4a3774367122527023c4c0043043b970c171d16c
k4rtowka <k4rtowkayt@gmail.com>
2023-09-28 13:25:28 +0300

    Начало проекта

---
+++ src/main/java/Models/Entity.java
@@ -0,0 +1,11 @@
+public abstract class Entity {
+    //region Поля
+    private int DPS;
+    private int DEF;
+    private int HP;
+    //endregion
+
+    //region Con
+
+    //endregion
+}
---
+++ src/main/java/Models/Player.java
@@ -0,0 +1,15 @@
+package Models;
+
+import Models.Entity;
+
+public class Human extends Entity {
+
+    public Human(int DPS, int DEF, int HP){
+        super(DPS, DEF, HP);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
